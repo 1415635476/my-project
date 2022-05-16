1b1872feaaa8f0fc7082e3c1d906865ca0fbedb9
小小yl <2697140121@qq.com>
2022-05-16 18:07:39 +0800

    master提交

--- src/Test2.java
+++ src/Test2.java
@@ -4,6 +4,8 @@ public class Test2 {
         System.out.println("master分支提交");
         System.out.println("dev分支第一次提交");
         System.out.println("dev-gym分支第一次提交");
+
+        System.out.println("master提交");
     }
 
 }
