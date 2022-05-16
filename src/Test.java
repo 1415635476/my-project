367784814f7c9b487dc45f7a7f2365e78bdc8784
小小yl <2697140121@qq.com>
2022-05-16 12:43:15 +0800

    第一次提交

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("5月16号星期一");
+        System.out.println("5月16号星期一");
+        System.out.println("5月16号星期一");
+    }
+
+}
